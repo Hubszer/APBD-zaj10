c8855b7ce3f0636b04fb7698d107e9429ff3c417
Hubert Materek <s25160@pjwstk.edu.pl>
2024-06-04 11:07:40 +0200

    na lekcji

---
+++ CodeFirst/Migrations/20240604083856_Init2.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CodeFirst.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "authors",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, "John", "Doe" },
+                    { 2, "Ann", "Smith" },
+                    { 3, "Jack", "Taylor" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "authors",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "authors",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "authors",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
