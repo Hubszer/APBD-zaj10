97c7d06a1a574a2a395b59492dd1ad01dcc43990
Hubszer <s25160@pjwstk.edu.pl>
2024-06-10 23:01:34 +0200

    donev2

---
+++ Zad10/Zad10/Program.cs
@@ -0,0 +1,25 @@
+
+using Microsoft.EntityFrameworkCore;
+using Zad10;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationContext>(
+    options => options.UseSqlServer("Name=ConnectionStrings:Default"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
